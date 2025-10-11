b92db355bc7794da37bbbcc046ddb6426caa2919
Dan Daogaru <d.daogaru@gmail.com>
2025-10-11 09:19:57 -0400

    Add build and SSG manifests for static assets

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R0yd5rwLuLtfE2icmTUhVl8s3Qn4tcFWSy+P+XGFROA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
