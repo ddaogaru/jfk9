c0b69dd1999031de608fff59bd119700ff76461d
Dan Daogaru <d.daogaru@gmail.com>
2025-10-18 19:42:24 -0400

    Refactor server reference manifest to use environment variable for encryption key
    - Updated the server reference manifest to replace the hardcoded encryption key with the environment variable `NEXT_SERVER_ACTIONS_ENCRYPTION_KEY`.
    - Simplified the JSON structure in the server reference manifest file for consistency.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lJMS1abAraSiZk98w4HluN5IlNwTImaGmKMOZwzohxc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
