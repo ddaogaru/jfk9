8a685cf716a9f91a10bd8e4bdeb30e71cc779472
Dan Daogaru <d.daogaru@gmail.com>
2025-10-21 16:42:27 -0400

    Switch from Google Tag Manager to Google Analytics with full consent permissions

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils';
 import ScrollIndicator from '@/components/ui/scroll-indicator';
 import PerformanceMonitor from '@/components/performance-monitor';
 import ServiceWorkerRegister from '@/components/service-worker-register';
+import ConsentManager from '@/components/consent-manager';
 import { Toaster } from '@/components/ui/sonner';
 import { siteConfig } from '@/config/site';
 import { LOCAL_BUSINESS_SCHEMA, ORGANIZATION_SCHEMA } from '@/lib/seo';
@@ -14,10 +15,10 @@ import '@/styles/globals.css';
 import '@/styles/custom.css';
 import 'sonner/dist/styles.css';
 
-// Google Tag Manager Container ID. To enable tracking, set NEXT_PUBLIC_GTM_CONTAINER_ID.
-const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID ?? 'GTM-KPM94DCS';
+// Google Analytics Measurement ID. To enable tracking, set NEXT_PUBLIC_GA_MEASUREMENT_ID.
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? 'G-JLCJXY85N3';
 const IS_PROD = process.env.NODE_ENV === 'production';
-const SHOULD_LOAD_GTM = IS_PROD && GTM_CONTAINER_ID.length > 0;
+const SHOULD_LOAD_GA = IS_PROD && GA_MEASUREMENT_ID.length > 0;
 
 const STRUCTURED_DATA = {
   '@context': 'https://schema.org',
@@ -87,13 +88,11 @@ export default async function RootLayout({
         {/* DNS prefetch for external resources */}
   <link rel="dns-prefetch" href="//fonts.googleapis.com" />
   <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-  <link rel="dns-prefetch" href="//www.googletagmanager.com" />
   <link rel="dns-prefetch" href="//www.google-analytics.com" />
   <link rel="dns-prefetch" href="//www.instagram.com" />
   <link rel="dns-prefetch" href="//www.facebook.com" />
         
     {/* Preconnect to GA endpoints for faster tag load */}
-  <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="anonymous" />
   <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="anonymous" />
         
         {/* Manifest for PWA capabilities */}
@@ -133,8 +132,8 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(LOCAL_BUSINESS_SCHEMA) }}
         />
 
-        {/* Google Tag Manager - Consent Mode v2 (All permissions granted) */}
-        {SHOULD_LOAD_GTM && (
+        {/* Google Analytics with Consent Mode v2 (All permissions granted) */}
+        {SHOULD_LOAD_GA && (
           <>
             <script
               dangerouslySetInnerHTML={{
@@ -154,13 +153,14 @@ export default async function RootLayout({
               }}
             />
             <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <script
               dangerouslySetInnerHTML={{
                 __html: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}');
                 `
               }}
             />
@@ -173,17 +173,6 @@ export default async function RootLayout({
           inter.className,
         )}
       >
-        {/* Google Tag Manager (noscript) */}
-        {SHOULD_LOAD_GTM && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
-        )}
 
         {/* Skip Navigation for Accessibility */}
         <a
@@ -197,6 +186,7 @@ export default async function RootLayout({
         <ScrollIndicator />
         <PerformanceMonitor />
         <ServiceWorkerRegister />
+        <ConsentManager />
         <Toaster />
       </body>
     </html>
